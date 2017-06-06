c3fbc2b7365acb2f6eac279c2804f6be67ffe0d5
Dominique Luna <dluna132@gmail.com>
2017-06-06 15:58:15 -0700

    Update install-ubuntu.sh
    update linux

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
-sudo apt-get install libuv1-dev libssl-dev
+sudo apt-get update
+sudo apt-get install git libuv1-dev libssl-dev gcc g++ cmake make
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
-# git checkout e94b6e1
+git checkout e94b6e1
 mkdir build
 cd build
 cmake ..
 make 
 sudo make install
-cd ..
-cd ..
+cd ../..
 sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
 sudo rm -r uWebSockets
